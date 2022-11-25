3f5296e2682335a0d06c8194033c42dae371c291
Amir Rastegar <rastegar.amir3@gmail.com>
2022-11-25 04:25:35 +0330

    init

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using framework.App.Adapters;
+using framework.App.Adapters.Services;
+
+namespace framework_test
+{
+    class Program
+    {
+        static void Main(string[] args){
+            BlueG.ClientId = Environment.GetEnvironmentVariable("client_id") ?? String.Empty;
+            BlueG.ClientSecret = Environment.GetEnvironmentVariable("client_secret") ?? String.Empty;
+
+            var service = new BlueGServices();
+            service.Achievement = new Achievements();
+            service.Leaderboard = new Leaderboard();
+            service.Chats = new Chats();
+            service.Friendship = new Friendship();
+            service.Party = new Party();
+            service.Users = new Users();
+
+            BlueG.Auth = new Auth();
+            BlueG.Table = new Table(service);
+            BlueG.Storage = new Storage();
+            BlueG.Start();
+            
+        }
+    }
+}
